42379ce261d3d6c757d69a5d011c35f8f0461e35
no1xsyzy <mf20070535@126.com>
2017-08-29 12:19:31 +0800

    Add info in Homepage

--- src/components/Homepage.vue
+++ src/components/Homepage.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="homepage">
-    <h1>Welcome to Homepage</h1>
-    <h2>This site is for demo of Vue components</h2>
+    <h1>Welcome to vue.inqb.ga</h1>
+    <h2>This site is for demo of <a href="https://vuejs.org/">Vue.js</a> components</h2>
     <h3><a href="https://github.com/no1xsyzy/vue-utils">Github repo</a></h3>
   </section>
 </template>
