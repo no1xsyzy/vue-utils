addda62ebab2cf9b3f1ae5105dffe98949a8735c
no1xsyzy <mf20070535@126.com>
2017-08-29 14:17:27 +0800

    Using worktree

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,9 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, '../vue-utils-dist/dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
